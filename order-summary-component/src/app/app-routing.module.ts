51407fbf03f81fb4553c287c6a4ce3c1b7ec7871
athira@voiro.com <kaithakkottuathira@gmail.com>
2022-04-11 14:24:41 +0530

    Page not  found

--- order-summary-component/src/app/app-routing.module.ts
+++ order-summary-component/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path:'',
     component:OrderSummaryComponent
+  },
+  {
+    path: '**', 
+    component: PageNotFoundComponent
   }
 ];
 
--- order-summary-component/src/app/app.module.ts
+++ order-summary-component/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
